49c21de0483a105b65dabe20140ac265713cb42d
Maxthon Chan <xcvista@me.com>
2013-07-09 10:10:18 +0800

    Starting: WebUIKit

---
+++ WebUIKit/WebUIKit.h
@@ -0,0 +1,13 @@
+//
+//  WebUIKit.h
+//  WebUIKit
+//
+//  Created by Maxthon Chan on 7/9/13.
+//  Copyright (c) 2013 muski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WebUIKit : NSObject
+
+@end
